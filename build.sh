2af458ea5f791cedb598a1a1810d614592fac6d9
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2019-12-01 10:07:35 +0100

    ci: update scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+set -e
+
 rm -f package-lock.json && npm install
-npm run build
+npm run build-dev
 npm run test
--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,37 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
+
+const production = process.env.NODE_ENV === "production";
+const excludedFiles = [
+  "!**/node_modules",
+  "!**/build.sh",
+  "!**/deploy.sh",
+  "!**/rollup.config.js",
+  "!index.src.js"
+];
 
 export default {
   input: "index.src.js",
   output: {
     sourcemap: false,
     format: "iife",
-    file: "index.js"
+    file: production ? "build/index.js" : "index.js"
   },
-  plugins: [babel(), resolve(), commonjs(), terser()]
+  plugins: [
+    babel(),
+    resolve(),
+    commonjs(),
+    terser(),
+    production &&
+      copy({
+        targets: [
+          {
+            src: ["**/*", "**/.*", ...excludedFiles],
+            dest: "build"
+          }
+        ]
+      })
+  ]
 };
